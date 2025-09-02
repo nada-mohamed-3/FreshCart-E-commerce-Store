4b831877072da871a748c74e99b28abbe067bf5d
nada-mohamed-3 <nadamoamed448@gmail.com>
2025-09-03 00:50:17 +0300

    fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'flowbite/dist/flowbite.js'
+import 'flowbite/dist/flowbite.css'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import './index.css'
